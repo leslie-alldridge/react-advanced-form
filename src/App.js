ba096affee50a08f1325d0f89f89da5c7ead27d9
leslie-alldridge <leslie.alldridge@gmail.com>
2019-02-25 11:31:35 +1300

    validation on forms

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { FormProvider } from 'react-advanced-form'
+import rules from './validation-rules'
+import messages from './validation-messages'
+import SyncValidation from './SyncValidation'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <FormProvider rules={rules} messages={messages}>
+    <SyncValidation />
+  </FormProvider>
+)
 
-export default App;
+export default App
